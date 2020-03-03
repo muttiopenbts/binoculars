a0d5ee1bcd8ad9a19658c999f60ecf89160194f4
mutti <mkocbayi@gmail.com>
2020-03-02 22:02:28 -0800

    Print() for python3

--- textify_function.py
+++ textify_function.py
@@ -11,31 +11,31 @@ class BinocularsTextifyFunction(BackgroundTaskThread):
 
 
     def textify_function_plain(self):
-        output = ''    
+        output = ''
         basic_blocks = sorted(self.function.basic_blocks, key=lambda bb: bb.start)
-        
+
         for basic_block in basic_blocks:
             for inst in basic_block.get_disassembly_text():
                 if str(inst.tokens[0]) == self.function.name: continue
-                
+
                 addr = hex(inst.address).replace("L", "")
                 offset = inst.address - self.function.start
                 output += "%s <%s+%d>:    %s\n" %\
                 (addr, self.function.name, offset, str(inst))
 
-        print output                
+        print(output)                
         show_plain_text_report("Binoculars Text Disasm", output)
 
 
     # TODO: syntax highlight
     def textify_function_html(self):
-        output = ''    
+        output = ''
         basic_blocks = sorted(self.function.basic_blocks, key=lambda bb: bb.start)
-        
+
         for basic_block in basic_blocks:
             for inst in basic_block.get_disassembly_text():
                 if str(inst.tokens[0]) == self.function.name: continue
-                
+
                 addr = hex(inst.address).replace("L", "")
                 offset = inst.address - self.function.start
                 output += "%s <%s+%d>:    %s\n" %\
@@ -49,13 +49,10 @@ class BinocularsTextifyFunction(BackgroundTaskThread):
         </body>
         </html>
         """ %(output)
-        print html                
-        show_html_report("Binoculars Text Disasm", output)        
+        print(html)
+        show_html_report("Binoculars Text Disasm", output)
 
 
     def run(self):
         #self.textify_function_html()
         self.textify_function_plain()
-
-
-
